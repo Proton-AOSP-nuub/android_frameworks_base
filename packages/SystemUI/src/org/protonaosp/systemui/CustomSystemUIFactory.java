67f14edca9852a8abd179166e5d0af105771dd20
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    ProtonSystemUI: smartspace and backgesture classifier integration
    Change-Id: I6de696d1273e7013ca1278911c53b4fcc35d04b8
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/6371
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- packages/SystemUI/src/org/protonaosp/systemui/CustomSystemUIFactory.java
+++ packages/SystemUI/src/org/protonaosp/systemui/CustomSystemUIFactory.java
@@ -2,26 +2,46 @@ package org.protonaosp.systemui;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.os.Handler;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
 
 import org.protonaosp.systemui.dagger.DaggerGlobalRootComponentProton;
 import org.protonaosp.systemui.dagger.GlobalRootComponentProton;
+import org.protonaosp.systemui.dagger.SysUIComponentProton;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
 import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class CustomSystemUIFactory extends SystemUIFactory {
-    
+
     // ML back gesture provider
     @Override
     public BackGestureTfClassifierProvider  createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
         return new CustomBackGestureTfClassifierProvider(am, modelName);
     }
-        
+
     @Override
     protected GlobalRootComponent buildGlobalRootComponent(Context context) {
         return DaggerGlobalRootComponentProton.builder()
                 .context(context)
                 .build();
+    }
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentProton) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
