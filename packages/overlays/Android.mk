e08cf87f1a3c7e74d802d984c7b5984aafb03049
Pranav Vashi <neobuddy89@gmail.com>
2022-02-13 05:33:34 +0530

    overlays: Add Outline iconpack
    Change-Id: I85a93cd775a62b3b14f2a705da6da561e6c9786a

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -62,6 +62,9 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackOOSSettingsOverlay \
 	IconPackOOSSystemUIOverlay \
 	IconPackOOSThemePickerOverlay \
+	IconPackOutlineAndroidOverlay \
+	IconPackOutlineSettingsOverlay \
+	IconPackOutlineSystemUIOverlay \
 	IconShapeCloudyOverlay \
 	IconShapeCylinderOverlay \
 	IconShapeFlowerOverlay \
